c395e94901d7783b3291e647f6ff596d97008bd7
Justin Forlenza <justin@justinforle.nz>
2021-11-21 15:03:08 -0500

    Added projects page

---
+++ src/pages/Projects.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>/projects</h1>
+  <p>Here is a sampling of my work, both open source and for private clients</p>
+  <h2>Open Source</h2>
+  <ul>
+    <li>
+      <a href="https://github.com/justinforlenza/keylight-control" target="_blank">Keylight Control</a> -
+      A NodeGUI powered cross platform application to control Elgato's Keylight products
+    </li>
+    <li>
+      <a href="https://github.com/justinforlenza/gsuite-tool" target="_blank">GSuite Tool</a> -
+      An electron desktop application designed to aid in a Google Workspace (formally GSuite) user administration tasks
+    </li>
+  </ul>
+  <h2>Private Work</h2>
+  <ul>
+    <li>
+      <a href="https://brooklynnavyyard.org" target="_blank">Brooklyn Navy Yard</a> -
+      Developed a central hub for the Navy Yard's tenants to manage their employees, and their access to the Yard's services
+    </li>
+    <li>
+      <a href="https://nyc.gov/cyber" target="_blank">NYC Cyber Command</a> -
+      Developed numerous web applications to aid in the prevention, detection and remediation of cyber threats
+    </li>
+  </ul>
+</template>
+
+<style>
+
+</style>
