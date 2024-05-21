3496e470ddd2e14dd7634b5669ae296491dfae7a
Justin Forlenza <justin@forlenza.co>
2024-05-21 18:29:15 -0400

    created layout

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from 'unocss'
+import transformerVariantGroup from '@unocss/transformer-variant-group'
 
 export default defineConfig({
+  
+  transformers: [
+    transformerVariantGroup(),
+  ],
   
 })
