e268828b1a2c63618ebd68c6a4b18c2a0902d688
Justin Forlenza <justin@forlenza.co>
2024-05-21 17:35:24 -0400

    added alpinejs

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config'
+
 import UnoCSS from 'unocss/astro'
+
+import alpine from '@astrojs/alpinejs'
 
 export default defineConfig({
   integrations: [
     UnoCSS({
       injectReset: true,
+    }),
+    alpine({
+      
     }),
   ],
 })
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,7 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+
+declare interface Window { 
+  Alpine: import('@types/alpinejs').Alpine
+}
